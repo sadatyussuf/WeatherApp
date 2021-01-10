b785d08e71163c528715250caf5495f916693082
sadatyussuf <tommyleespartan0@gmail.com>
2021-01-10 11:24:21 +0000

    first commit

---
+++ open weather1/script.js
@@ -0,0 +1,58 @@
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.form-control');
+const row = document.querySelector('.rows')
+
+const url = "http://api.openweathermap.org/data/2.5/weather?q="
+const apiKey ="&appid=bfc7de9a9c74f31c8d56bfe3ce52dcd9"
+const unit = "&units=metric"
+
+
+
+
+btn.addEventListener('click',()=>{
+
+    const value = input.value;
+
+    var requestOptions = {
+        method: 'GET'
+        };
+
+    fetch(url+value+apiKey+unit, requestOptions)
+        .then(response => response.json())
+        .then(result => { 
+            console.log(result)//result.weather[0]['description']
+        //style="width: 18rem;"
+            const output = `
+
+        <div class="card text-center" >
+
+            <div class="card-body">
+                <h5 class="card-title">${result.name} <sup>${result.sys['country']}</sup></h5>
+
+                <h6>${result.main['temp']}<sup>°C</sup></h6>
+
+                <div><img src="http://openweathermap.org/img/w/${result.weather[0]['icon']}.png"></div>
+
+
+                <p class="card-text">${result.weather[0]['description']}</p>
+            
+            </div>
+
+        </div>
+
+            `
+        
+        
+        row.innerHTML += output
+
+        })
+        .catch(error => console.log('error', error));
+
+input.value = ''
+
+
+
+
+})
+
+
